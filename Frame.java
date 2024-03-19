8c0b8b66303ee7eae5a1bc4b6b86ae1b99559e06
Logan Bechtel <bechtello25@spaldinghs.org>
2024-03-19 13:45:54 -0400

    Fix: Frame.java formatting

--- Frame.java
+++ Frame.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 public class Frame {
 
@@ -17,21 +18,34 @@ public class Frame {
         JLabel label2 = new JLabel("Current Season: " + firstInfo.getCurrentGameName() + " - " + firstInfo.getCurrentSeason());
         JLabel label3 = new JLabel();
 
-        JTextArea compInfo = new JTextArea(compSchedule.get4541CompSchedule(eventID));
+        JTextArea compInfo = new JTextArea(CompSchedule.get4541CompSchedule(eventID));
 
-        Font customFont = new Font("Trebuchet MS", Font.BOLD, 20);
-        label1.setFont(customFont);
-        label2.setFont(customFont);
-        label3.setFont(customFont);
-        compInfo.setFont(customFont);
+        JPanel panel = new JPanel(null) {
 
-        label1.setBounds(750, 0, 500, 30); 
-        label2.setBounds(780, 20, 500, 30);
-        label3.setBounds(880, 40, 500, 30); 
-        compInfo.setBounds(50, 70, 1600, 600); 
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
 
+                GradientPaint gradient = new GradientPaint(100, 100, new Color(45, 45, 45), getWidth(), getHeight(), new Color(45, 45, 45));
 
-        JPanel panel = new JPanel(null);
+                ((Graphics2D) g).setPaint(gradient);
+
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+
+        setVisuals(label1, label2, label3, compInfo);
+        setPositions(label1, label2, label3, compInfo);
+        addComponents(label1, label2, label3, compInfo, panel, frame);
+
+        while (true) {
+            label3.setText(Clock.getCurrentTime());
+            compInfo.setText(CompSchedule.get4541CompSchedule(eventID));
+        }
+    }
+
+    public void addComponents(JLabel label1, JLabel label2, JLabel label3, JTextArea compInfo, JPanel panel, JFrame frame) {
+
         panel.add(label1);
         panel.add(label2);
         panel.add(label3);
@@ -40,13 +54,35 @@ public class Frame {
         frame.add(panel);
 
         frame.setSize(1920, 1080);
-
         frame.setVisible(true);
-
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-        while(true) {
-            label3.setText(Clock.getCurrentTime());
-        }
+    public void setVisuals(JLabel label1, JLabel label2, JLabel label3, JTextArea compInfo) {
+
+        Font introFont = new Font("Trebuchet MS", Font.BOLD, 30);
+        Font clockFont = new Font("Trebuchet MS", Font.BOLD, 50);
+        Font compInfoFont = new Font("Trebuchet MS", Font.BOLD, 20);
+
+        label1.setFont(introFont);
+        label2.setFont(introFont);
+        label3.setFont(clockFont);
+        compInfo.setFont(compInfoFont);
+
+        label1.setForeground(Color.WHITE);
+        label2.setForeground(Color.WHITE);
+        label3.setForeground(Color.WHITE);
+        compInfo.setForeground(Color.WHITE);
+
+        compInfo.setBackground(Color.DARK_GRAY);
+        compInfo.setBorder(new LineBorder(Color.WHITE, 2));
+    }
+
+    public void setPositions(JLabel label1, JLabel label2, JLabel label3, JTextArea compInfo) {
+
+        label1.setBounds(560, 0, 750, 30);
+        label2.setBounds(600, 30, 750, 30);
+        label3.setBounds(700, 60, 500, 50);
+        compInfo.setBounds(50, 120, 1600, 600);
     }
 }
